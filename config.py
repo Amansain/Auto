6978d4e87c8cb23de8daed417632a48a08c5f932
Amansain <91235289+Amansain@users.noreply.github.com>
2023-10-12 20:56:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ import logging
 
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", "7822571"))
-    API_HASH = os.environ.get("API_HASH", "067329e70bfc5b3022f77080703da788")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "2089413356:AAFkYhLxRqwQaR0QHk2czfYQHWI8TAy-Hf0") 
+    API_ID = int(os.environ.get("API_ID", "15951945"))
+    API_HASH = os.environ.get("API_HASH", "7b82b801d197921e54cf404799ae5b3f")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5951942864:AAE_sZsQG9vo1NUQc8oTDzxBwD-veRcBWEE") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
     CAPTION = os.environ.get("CAPTION", """<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <code>{file_name}</code>
 
@@ -25,7 +25,7 @@ class Config:
     OWNER_ID = os.environ.get("OWNER_ID", 12345)
     LIMIT = int(os.environ.get("LIMIT", "25000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
-    SESSION = os.environ.get("SESSION", "BQCenDdoZsPgoohWSS4Up3743sPNtcVEZ2-QUDsWzstPQD-JL-KjqRvbCEQqEeailA-q4Jid-8bLYKbVl7xGVWZTD8YugCHjpoSlTE4Wm6uWZRCC3Eut4iP0MoyysR19OMdC1ZRabY_AzW0QEj8N-Xmvd9G1dy82TvSSV-HTUNS5gOpTBXCXi3xXt9Y0X1gXlODz4wA9hNl8UsulpAzuNIEE6tdRwmyFgtf41MTrc-FXwxQyq0pq2A1fOo0V2JiWmwrD1ID1gAF-xRILOgkAmeHu43cZPyUjqnSTng-hOKAZah94TQCVH2EhZ-qhyNocWtap1lBmx_LU_ZxdW6cpI8PBccJeTwA")
+    SESSION = os.environ.get("SESSION", "BQDzaEkAs9UhTzXX-uRHZQD0Dh-paB_U16uZalgLN_IoMEZB-PSNFO8wO-a610oSYMvD7JQ1r61bc_toZR5FOYATHY2IQuulFVFl9_xqP7uwg52HpwZZEGGgb6NjYCpqb1vppXF4orhDxwJwKT11VxFQZvrDAnrZ0qVPFKKDQR9RgodMgueF-P2FwW4E8mPgV9juEr63RkqwwqKx6CvHbJdCZ87JyA6K6i8zlaIq0UcGGmiXKU8eiAz3iaZh8_g8r8_gekEmblMh-TvjHFoi2-GaI2p8HtNbAyEkhkjaEOQS4lLcGcTJGYYguTbq6zyCJvrxRiXmJjQwU7rTF83uxQs0QUuyPAAAAAFEMvphAA")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", 12345))
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
